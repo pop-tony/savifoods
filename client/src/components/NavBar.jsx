dffd4b58c8b857585daa94f1ff6ef924987547a8
poptonydm <poptonydm@gmail.com>
2026-05-30 03:31:28 -0800

    added react in navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-// src/components/Navbar.jsx
+import React from 'react'
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
